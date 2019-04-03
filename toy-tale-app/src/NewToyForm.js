77528e0cf3ca697b516d723bcfe862d4edbbe191
spikeburton <ralphburton3@gmail.com>
2019-04-03 15:13:20 -0400

    render new toy form

--- toy-tale-app/src/NewToyForm.js
+++ toy-tale-app/src/NewToyForm.js
@@ -1,13 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class NewToyForm extends Component {
   render() {
     return (
-      <div className="container" style={{display: "block"}}>
-        <form className="add-toy-form"></form>
+      <div className="container" style={{ display: "block" }}>
+        <form className="add-toy-form">
+          <h3>Create a toy!</h3>
+          <input
+            type="text"
+            name="name"
+            value=""
+            placeholder="Enter a toy's name..."
+            class="input-text"
+          />
+          <input
+            type="text"
+            name="image"
+            value=""
+            placeholder="Enter a toy's image URL..."
+            class="input-text"
+          />
+          <br/>
+          <input
+            type="submit"
+            name="submit"
+            value="Create New Toy"
+            class="submit"
+          />
+        </form>
       </div>
-    )
+    );
   }
 }
 
-export default NewToyForm
+export default NewToyForm;
