e9f2d5d2789effc6d2bfd7c7cf00293670818f45
spikeburton <ralphburton3@gmail.com>
2019-04-03 16:19:49 -0400

    implement functionality for adding a new toy

--- toy-tale-app/src/App.js
+++ toy-tale-app/src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
-import ToyHeader from './ToyHeader';
-import NewToyForm from './NewToyForm';
-import AddToyButton from './AddToyButton';
-import ToyCollection from './ToyCollection'
+import React, { Component } from "react";
+import ToyHeader from "./ToyHeader";
+import NewToyForm from "./NewToyForm";
+import AddToyButton from "./AddToyButton";
+import ToyCollection from "./ToyCollection";
 
-import { toys } from './resources/toys';
+import { toys } from "./resources/toys";
 
 class App extends Component {
   constructor() {
@@ -12,33 +12,45 @@ class App extends Component {
     this.state = {
       displayForm: false,
       toys: toys
-    }
+    };
   }
 
   handleNewToyButton = () => {
     this.setState({
       displayForm: !this.state.displayForm
-    })
-  }
+    });
+  };
 
   handleNewToyForm = (e, name, image) => {
     e.preventDefault();
-    console.log("trying to add toy ...")
-    console.log(name)
-    console.log(image)
-    e.target.reset()
-  }
+
+    const newToy = {
+      id: this.state.toys[this.state.toys.length - 1].id + 1,
+      name,
+      image,
+      likes: null
+    };
+
+    this.setState({
+      toys: [...this.state.toys, newToy]
+    });
+
+    e.target.reset();
+  };
 
   render() {
     return (
       <div>
         <ToyHeader />
-        <NewToyForm display={this.state.displayForm} handleSubmit={this.handleNewToyForm} />
+        <NewToyForm
+          display={this.state.displayForm}
+          handleSubmit={this.handleNewToyForm}
+        />
         <AddToyButton handleClick={this.handleNewToyButton} />
         <ToyCollection toys={this.state.toys} />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
