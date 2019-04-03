301c53b9bfc64f01b71f0816ba95e7aa48ece0df
spikeburton <ralphburton3@gmail.com>
2019-04-03 15:40:10 -0400

    add likes for ToyCard

--- toy-tale-app/src/ToyCard.js
+++ toy-tale-app/src/ToyCard.js
@@ -4,13 +4,13 @@ class ToyCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      likes: this.props.likes
+      likes: this.props.likes || 0
     };
   }
 
   handleClick = () => {
-    console.log('button was clicked')
-    const likes = this.state.likes || 0;
+    const likes = this.state.likes;
+    console.log(likes)
     this.setState({
       likes: likes + 1
     });
@@ -20,7 +20,7 @@ class ToyCard extends Component {
       <div className="card">
         <h2>{this.props.name}</h2>
         <img src={this.props.image} className="toy-avatar" alt="toy-avatar" />
-        <p>{this.props.likes ? `${this.state.likes} Likes` : `0 Likes`}</p>
+        <p>{this.state.likes === 1 ? "1 Like" : `${this.state.likes} Likes`}</p>
         <button className="like-btn" onClick={this.handleClick}>
           {"Like <3"}
         </button>
